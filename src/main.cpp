80841f894123397878c5a6dd824f7cc94820c40f
jaisor <jaisor@gmail.com>
2021-03-25 14:16:53 -0700

    Honey orange palette

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,11 @@
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 
+#define HONEY_DARK_ORANGE 0x5c2b00
+#define HONEY_ORANGE 0xff8400
+#define HONEY_LIGHT_ORANGE 0xffaa00
+#define HONEY_YELLOW 0xffcc00
+
 CRGB leds[NUM_LEDS];
 
 #define UPDATES_PER_SECOND 50
@@ -17,12 +22,29 @@ CRGB leds[NUM_LEDS];
 CRGBPalette16 currentPalette;
 TBlendType currentBlending;
 
+
+
 extern const TProgmemRGBPalette16 HoneyOrange_p FL_PROGMEM =
 {
-    0xff623b,
-    0xff8400,
-    0xffd321,
-    0xff8400
+    HONEY_DARK_ORANGE,
+    HONEY_DARK_ORANGE,
+    HONEY_DARK_ORANGE,
+    HONEY_DARK_ORANGE,
+
+    HONEY_ORANGE,
+    HONEY_ORANGE,
+    HONEY_ORANGE,
+    HONEY_ORANGE,
+
+    HONEY_LIGHT_ORANGE,
+    HONEY_LIGHT_ORANGE,
+    HONEY_LIGHT_ORANGE,
+    HONEY_LIGHT_ORANGE,
+    
+    HONEY_YELLOW,
+    HONEY_YELLOW,
+    HONEY_YELLOW,
+    HONEY_YELLOW,
 };
 
 void fillLEDsFromPaletteColors( uint8_t colorIndex)
